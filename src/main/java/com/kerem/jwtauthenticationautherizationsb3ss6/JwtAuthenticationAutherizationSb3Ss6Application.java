e84f65b0090e77ef2f20522125a4e8e10549ab0f
keremersann <keremersan01@gmail.com>
2023-05-30 12:15:02 +0300

    Entities are added

---
+++ src/main/java/com/kerem/jwtauthenticationautherizationsb3ss6/JwtAuthenticationAutherizationSb3Ss6Application.java
@@ -0,0 +1,13 @@
+package com.kerem.jwtauthenticationautherizationsb3ss6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAutherizationSb3Ss6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationAutherizationSb3Ss6Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/kerem/jwtauthenticationautherizationsb3ss6/entity/Role.java
@@ -0,0 +1,5 @@
+package com.kerem.jwtauthenticationautherizationsb3ss6.entity;
+
+public enum Role {
+    ADMIN,USER
+}
